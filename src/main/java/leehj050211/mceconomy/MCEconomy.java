1efa89601842a74de7aa13259fa9bd7f7e83d732
leehj050211 <leehj050211@gmail.com>
2023-08-17 21:35:40 +0900

    Initial Commit

---
+++ src/main/java/leehj050211/mceconomy/MCEconomy.java
@@ -0,0 +1,17 @@
+package leehj050211.mceconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MCEconomy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
