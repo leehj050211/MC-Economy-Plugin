ca65f87bd5b356fce5356544b3456a9eb4d06686
leehj050211 <leehj050211@gmail.com>
2023-08-23 16:14:43 +0900

    REMOVE: 필요없는 코드 삭제

--- src/main/java/leehj050211/mceconomy/constant/MenuId.java
+++
@@ -1,9 +0,0 @@
-package leehj050211.mceconomy.constant;
-
-public enum MenuId {
-    SELECT_JOB,
-    SELECT_GACHA,
-    SELECT_SHOP_CATEGORY,
-    SELECT_SHOP_ITEM_CATEGORY,
-    SELECT_SHOP_ITEM
-}
