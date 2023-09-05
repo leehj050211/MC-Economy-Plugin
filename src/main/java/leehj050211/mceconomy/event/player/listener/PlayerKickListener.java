d23607ae15ec44383106a62ae203a444d68b0533
min050410 <min050410@gmail.com>
2023-09-05 14:17:10 +0900

    add: BlockBreakListener

--- src/main/java/leehj050211/mceconomy/event/player/listener/PlayerKickListener.java
+++ src/main/java/leehj050211/mceconomy/event/player/listener/PlayerKickListener.java
@@ -1,5 +1,7 @@
 package leehj050211.mceconomy.event.player.listener;
 
+import leehj050211.mceconomy.domain.player.PlayerData;
+import leehj050211.mceconomy.global.exp.JobExpManager;
 import leehj050211.mceconomy.global.player.PlayerManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -10,9 +12,14 @@ public class PlayerKickListener implements Listener {
 
     private final PlayerManager playerManager = PlayerManager.getInstance();
 
+    private final JobExpManager jobExpManager = JobExpManager.getInstance();
+
     @EventHandler
     public void onPlayerKickEvent(PlayerKickEvent event) {
         Player player = event.getPlayer();
+        PlayerData playerData = playerManager.getData(player.getUniqueId());
+
         playerManager.deletePlayer(player);
+        jobExpManager.deletePlayer(playerData);
     }
 }
